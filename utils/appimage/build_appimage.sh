89fa16e33261efe0032227ca55a10cb6d8c10355
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-04-25 02:46:46 +0200

    fix change dir

--- utils/appimage/build_appimage.sh
+++ utils/appimage/build_appimage.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
-wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage" -O linuxdeploy-x86_64.AppImage
-wget -c "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage" -O linuxdeploy-plugin-qt-x86_64.AppImage
-chmod +x linuxdeploy-x86_64.AppImage
-chmod +x linuxdeploy-plugin-qt-x86_64.AppImage
 
 
 SCRIPT_DIR="$(pwd)/build"
 M_BUILD_PWD="$SCRIPT_DIR/monitor-bin"
 BIN_PATH="$M_BUILD_PWD/bin/rcssmonitor"
 APP_IMAGE_DIR="$SCRIPT_DIR/appimage"
+
+cd $SCRIPT_DIR
+
+wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage" -O linuxdeploy-x86_64.AppImage
+wget -c "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage" -O linuxdeploy-plugin-qt-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-plugin-qt-x86_64.AppImage
+
 
 rm -rf $APP_IMAGE_DIR || true
 mkdir -p $APP_IMAGE_DIR || true
@@ -20,7 +24,6 @@ mkdir -p $APP_IMAGE_DIR || true
 LIBZ_PATH=$(ldd $BIN_PATH | grep libz.so | awk '{ print $3 }')
 LIBRCSSRCG_PATH=$(ldd $BIN_PATH | grep librcssrcg.so | awk '{ print $3 }')
 
-cd $SCRIPT_DIR
 ./linuxdeploy-x86_64.AppImage --appdir $APP_IMAGE_DIR \
                 --plugin qt \
                 -l $LIBZ_PATH \
